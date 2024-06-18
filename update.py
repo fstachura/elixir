f32755c9ce06f657cd966663314031fbd97ee5cb
Franciszek Stachura <fbstachura@gmail.com>
2024-06-18 14:42:20 +0200

    Add a list of tokens that are always indexed
    Currently, the list consists of arbitrarily selected tokens from
    c_common_reswords list present in GCC source code.
    
    https://gcc.gnu.org/git/?p=gcc.git;a=blob;f=gcc/c-family/c-common.cc;h=0341c44a2cd99771c3eeb44878d3df7a9ae816ea;hb=HEAD#l385

--- update.py
+++ update.py
@@ -326,11 +326,12 @@ class UpdateRefs(Thread):
                     if even:
                         tok = prefix + tok
 
-                        if (db.defs.exists(tok) and
-                            not ( (idx*idx_key_mod + line_num) in defs_idxes and
-                                defs_idxes[idx*idx_key_mod + line_num] == tok ) and
-                            (family != 'M' or tok.startswith(b'CONFIG_'))):
-                            # We only index CONFIG_??? in makefiles
+                        ref_allowed = db.defs.exists(tok) or (tok in always_indexed_tokens)
+                        # We only index CONFIG_??? in makefiles
+                        config_or_not_makefile = tok.startswith(b'CONFIG_') or family != 'M'
+                        i = idx*idx_key_mod + line_num
+
+                        if ref_allowed and defs_idxes.get(i) != tok and config_or_not_makefile:
                             if tok in idents:
                                 idents[tok] += ',' + str(line_num)
                             else:
