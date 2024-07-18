0334a0be60bef6dcf0e0693de28bfb62448a90c1
Franciszek Stachura <fbstachura@gmail.com>
2024-07-18 14:21:01 +0200

    Remove current_url template variable
    Now that the base tag is gone it's not useful anymore

--- http/web.py
+++ http/web.py
@@ -320,7 +320,7 @@ def generate_source(q, path, version, tag, project):
     html_code_block = format_code(fname, code)
 
     # Replace line numbers by links to the corresponding line in the current file
-    html_code_block = sub('href="#-(\d+)', 'name="L\\1" id="L\\1" href="/'+project+'/'+version+'/source'+path+'#L\\1', html_code_block)
+    html_code_block = sub('href="#-(\d+)', 'name="L\\1" id="L\\1" href="#L\\1', html_code_block)
 
     for f in filters:
         if filter_applies(f, path):
@@ -425,8 +425,6 @@ def generate_source_page(q, basedir, parsed_path):
     data = {
         **template_ctx,
 
-        'current_url': stringify_source_path(parsed_path),
-
         'current_project': project,
         'current_tag': tag,
         'current_tag_urlencoded': version,
@@ -510,8 +508,6 @@ def generate_ident_page(q, basedir, parsed_path):
     title_suffix = project.capitalize()+' source code ('+tag+') - Bootlin'
 
     data = {
-        'current_url': stringify_ident_path(parsed_path),
-
         'current_project': project,
         'current_tag': tag,
         'current_tag_urlencoded': version,
