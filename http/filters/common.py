15dd808e4d000b38b92b88ac4b4585e8806b952e
Franciszek Stachura <fbstachura@gmail.com>
2024-07-30 21:31:43 +0200

    Move DTS filters to new filter interface

--- http/filters/common.py
+++ http/filters/common.py
@@ -1,10 +1,20 @@
 # Common filters
 from filters.utils import encode_number, decode_number
 from filters.ident import IdentFilter
+from filters.dtscompB import DtsCompBFilter
+from filters.dtscompC import DtsCompCFilter
+from filters.dtscompD import DtsCompDFilter
 
 new_filters = [
     IdentFilter(),
 ]
+
+if dts_comp_support:
+    new_filters = [
+        DtsCompBFilter(),
+        DtsCompCFilter(),
+        DtsCompDFilter(),
+    ] + new_filters
+
 filters = []
-exec(open('dtscomp.py').read())
 exec(open('cppinc.py').read())
