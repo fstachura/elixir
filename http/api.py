e5e851b37c37f8ae3382ae4aaff42e36a46d73d2
Franciszek Stachura <fbstachura@gmail.com>
2024-08-07 13:21:02 +0200

    Move get_query to query.py
    Refactor api and autocomplete to use it and properly handle errors
    related to invalid project names.

--- http/api.py
+++ http/api.py
@@ -29,19 +29,14 @@ ELIXIR_DIR = os.path.dirname(os.path.realpath(__file__)) + '/..'
 if ELIXIR_DIR not in sys.path:
     sys.path = [ ELIXIR_DIR ] + sys.path
 
-import query
+from query import get_query
 
 class ApiIdentGetterResource:
     def on_get(self, req, resp, project, ident):
-        try:
-            basedir = req.env['LXR_PROJ_DIR']
-        except KeyError:
-            basedir = os.environ['LXR_PROJ_DIR']
-
-        data_dir = basedir + '/' + project + '/data'
-        repo_dir = basedir + '/' + project + '/repo'
-
-        q = query.Query(data_dir, repo_dir)
+        query = get_query(req.context.config.project_dir, project)
+        if not query:
+            resp.status = falcon.HTTP_NOT_FOUND
+            return
 
         if 'version' in req.params:
             version = req.params['version']
@@ -49,14 +44,14 @@ class ApiIdentGetterResource:
             raise falcon.HTTPMissingParam('version')
 
         if version == 'latest':
-            version = q.query('latest')
+            version = query.query('latest')
 
         if 'family' in req.params:
             family = req.params['family']
         else:
             family = 'C'
 
-        symbol_definitions, symbol_references, symbol_doccomments = q.query('ident', version, ident, family)
+        symbol_definitions, symbol_references, symbol_doccomments = query.query('ident', version, ident, family)
         resp.text = json.dumps(
             {
                 'definitions': [sym.__dict__ for sym in symbol_definitions],
@@ -65,5 +60,5 @@ class ApiIdentGetterResource:
             })
         resp.content_type = falcon.MEDIA_JSON
         resp.status = falcon.HTTP_200
-        q.close()
+        query.close()
 
