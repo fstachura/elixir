936f2e7e389e0f74b4dd3954425deb05f6ad6d71
Franciszek Stachura <fbstachura@gmail.com>
2024-09-27 22:11:00 +0200

    web: Add input reset button
    I tried type="reset", but it reverts to initial value instead of
    resetting.
    
    https://developer.mozilla.org/en-US/docs/Web/HTML/Element/input/reset

--- static/script.js
+++ static/script.js
@@ -318,4 +318,8 @@ document.addEventListener('DOMContentLoaded', _ => {
   setupAnchorOffsetHandler();
   setupGoToTop();
   fix301();
+
+  document.getElementById('clear-search').addEventListener('click',
+    _ => document.getElementById('search-input').value = ''
+  );
 });
